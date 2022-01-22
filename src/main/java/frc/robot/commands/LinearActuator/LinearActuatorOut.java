ef0540a3a2c581519ebf9b8a21a2d6db044eed3d
Eagle Engineering <74387887+Patrick62@users.noreply.github.com>
2022-01-22 15:37:03 -0800

    hi
    bye

--- src/main/java/frc/robot/commands/LinearActuator/LinearActuatorOut.java
+++ src/main/java/frc/robot/commands/LinearActuator/LinearActuatorOut.java
@@ -27,7 +27,7 @@ public class LinearActuatorOut extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    linearActuator.move(15);
+    linearActuator.move(0);
   }
 
   // Called once the command ends or is interrupted.
